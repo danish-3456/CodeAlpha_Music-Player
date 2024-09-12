2d991da4fbdd6d9bc263e669f84dce0b61a6fb2e
danish-3456 <danishkroner123456@gmail.com>
2024-09-12 20:06:30 +0530

    add initial files

---
+++ js/playlist.js
@@ -0,0 +1,82 @@
+// List of songs
+
+let songs = [
+  {
+    name: "Choo Lo",
+    artist: "The Local Train",
+    img: "Khadahun",
+    audio: "ChooLoo"
+  },
+  {
+    name: "Aao Milo Chalein",
+    artist: "Shaan",
+    img: "AaoMilochale",
+    audio: "Aaomilochl"
+  },
+  {
+    name: "Ilahi",
+    artist: "Arijit Singh",
+    img: "lahi",
+    audio: "Illahi"
+  },
+  {
+    name: "O Meri Laila",
+    artist: "Atif Aslam",
+    img: "LailaMajnu",
+    audio: "Lailamajnu"
+  },
+  {
+    name: "Tere Hawale",
+    artist: "Arijit Singh",
+    img: "tereHawale",
+    audio: "Terehawale"
+  },
+  {
+    name: "Yeh Dosti",
+    artist: "Kishore Kumar",
+    img: "Yehdosti",
+    audio: "yeh dosti"
+  },
+  {
+    name: "Pehle Bhi Main",
+    artist: "Vishal Mishra",
+    img: "Pehlebhimain",
+    audio: "Pehlebhim"
+  },
+  {
+    name: "Ranjhana",
+    artist: "A.R Rahman,Jaswinder",
+    img: "ranjhana",
+    audio: "Ranjhana"
+  },
+  {
+    name: "Tujhme Rab Dhikta Hai",
+    artist: "Roop Kumar Rathod",
+    img: "tujhmerabdhikta",
+    audio: "tjrdh"
+  },
+  {
+    name: "Kun Faya Kun",
+    artist: "A.R.Rahman,Mohit Chauhan, Javed Ali",
+    img: "kunfayakun",
+    audio: "kunfykun"
+  },
+  {
+    name: "Channa Mereya",
+    artist: "Arijit Singh",
+    img: "channamereya",
+    audio: "Channamereya"
+  },
+  {
+    name: "Tum ho",
+    artist: "Mohit Chauhan",
+    img: "Tumho",
+    audio: "Tum Ho"
+  },
+  {
+    name: "Naadan Parinde",
+    artist: "A.R Rahamn, Mohit Chauhan",
+    img: "rockstar",
+    audio: "nadanpar"
+  }, 
+]
